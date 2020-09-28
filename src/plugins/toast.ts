fe7ef8a32a271fbc611b9e62ac46de1fb3d8f000
igorp <igorpopryadukhin@gmail.com>
2020-09-28 17:43:51 +0300

    Remove semicolon

--- src/plugins/toast.ts
+++ src/plugins/toast.ts
@@ -1,9 +1,9 @@
-import Vue from 'vue';
-import Toast from 'vue-toastification';
-import 'vue-toastification/dist/index.css';
+import Vue from 'vue'
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
 // see: https://maronato.github.io/vue-toastification
 Vue.use(Toast, {
   transition: 'Vue-Toastification__bounce',
   maxToasts: 14,
   newestOnTop: true
-});
+})
