d26c26cadd9c6d06a6cd74966544aad7631560d2
IGOR <igorpopryadukhin@gmail.com>
2020-10-16 11:59:56 +0300

    Default parameters

--- src/plugins/toast.ts
+++ src/plugins/toast.ts
@@ -1,9 +1,17 @@
 import Vue from 'vue'
-import Toast from 'vue-toastification'
+import Toast, { POSITION } from 'vue-toastification'
 import 'vue-toastification/dist/index.css'
 // see: https://maronato.github.io/vue-toastification
 Vue.use(Toast, {
   transition: 'Vue-Toastification__bounce',
   maxToasts: 14,
-  newestOnTop: true
+  position: POSITION.TOP_RIGHT,
+  newestOnTop: true,
+  closeOnClick: true,
+  draggable: true,
+  draggablePercent: 0.6,
+  showCloseButtonOnHover: true,
+  hideProgressBar: true,
+  closeButton: 'button',
+  timeout: 3000
 })
