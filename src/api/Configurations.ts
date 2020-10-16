c900508287fa37926c6f98c5aa28b13a8ee58052
IGOR <igorpopryadukhin@gmail.com>
2020-10-16 11:45:49 +0300

    Api Configurations

---
+++ src/api/Configurations.ts
@@ -0,0 +1,42 @@
+import { $axios } from '@/plugins/axios'
+import { AxiosResponse } from 'axios'
+
+export interface ATEConfigurationInterface {
+  server: string;
+  display_name: string;
+  login: string;
+  password: string;
+}
+
+export class Configurations {
+  /**
+   * Receives ATE configuration
+   */
+  public getATEConfigurations (): Promise<ATEConfigurationInterface> {
+    return new Promise((resolve, reject) => {
+      $axios.get('/configurations/ate')
+        .then((response: AxiosResponse) => {
+          if (response.status !== 200) {
+            reject(response.data)
+          }
+          resolve(response.data)
+        }).catch(reject)
+    })
+  }
+
+  /**
+   * Setting up a configuration for ATE
+   * @param conf
+   */
+  public setATEConfigurations (conf: ATEConfigurationInterface): Promise<any> {
+    return new Promise((resolve, reject) => {
+      $axios.put('/configurations/ate', conf)
+        .then((response: AxiosResponse) => {
+          if ([200, 204].includes(response.status)) {
+            resolve(response.data)
+          }
+          reject(response.data)
+        }).catch(reject)
+    })
+  }
+}
