ee001c5beb52f6cce31f57e5e76feb91c4840ff0
IGOR <igorpopryadukhin@gmail.com>
2020-11-10 16:53:31 +0300

    Small fix contact list

--- src/views/Contacts/List.vue
+++ src/views/Contacts/List.vue
@@ -131,7 +131,7 @@
                 :key="`list-item-${item.id}`"
                 ripple
                 selectable
-                :to="{ name: 'contacts_view', params: { contact_id: item.id } }"
+                @click.stop="$router.push({ name: 'contacts_view', params: { contact_id: item.id } })"
               >
                 <v-list-item-action>
                   <v-checkbox
@@ -143,8 +143,8 @@
                   <v-list-item-title>
                     {{ item.first_name }} {{ item.last_name }}
                   </v-list-item-title>
-                  <v-list-item-subtitle v-if="item.phone_number_default">{{item.phone_number_default.type}}: {{ item.phone_number_default.value }}</v-list-item-subtitle>
-                  <v-list-item-subtitle v-else>Нет номера по умолчанию</v-list-item-subtitle>
+                  <v-list-item-subtitle v-if="item.default_phone">{{ item.default_phone.label }}: {{ item.default_phone.value.international }}</v-list-item-subtitle>
+                  <v-list-item-subtitle v-else>{{ $t('No default number') }}</v-list-item-subtitle>
                 </v-list-item-content>
                 <v-spacer />
                 <v-list-item-content>
@@ -154,16 +154,6 @@
                   >{{ item.user.first_name }} {{ item.user.last_name }}
                   </v-list-item-title>
                 </v-list-item-content>
-                <v-list-item-action>
-                  <v-btn
-                    icon
-                    large
-                    :disabled="!item.phone_number_default"
-                    @click.stop="$jsSIP.call(item.id, item.phone_number_default.value)"
-                  >
-                    <v-icon>mdi-phone</v-icon>
-                  </v-btn>
-                </v-list-item-action>
                 <v-list-item-action>
                   <v-menu offset-y>
                     <template v-slot:activator="{ on, attrs }">
@@ -255,174 +245,6 @@
         </v-col>
       </v-row>
     </v-card>
-
-    <!-- Dialog -->
-    <v-row justify="center">
-      <v-dialog
-        v-model="contactDialog.visible"
-        max-width="900"
-        persistent
-      >
-        <v-card class="overflow-hidden">
-          <v-app-bar
-            absolute
-            color="white"
-            height="80"
-            class="pr-4"
-            elevate-on-scroll
-          >
-            <v-app-bar-nav-icon class="ml-3" style="background-color: #8d3eb1; color: white">
-              {{ avatar }}
-            </v-app-bar-nav-icon>
-            <v-toolbar-title>
-              {{ contact.first_name }} {{ contact.last_name }}
-            </v-toolbar-title>
-            <v-spacer />
-            <v-btn
-              icon
-              :disabled="['accepted', 'call', 'connecting', 'progress'].includes($jsSIP.state)"
-              @click="contactDialog.visible = false"
-            >
-              <v-icon>mdi-close</v-icon>
-            </v-btn>
-          </v-app-bar>
-          <v-sheet
-            id="scrolling-techniques-7"
-            max-height="600"
-            style="margin-top: 80px"
-          >
-            <v-container class="pl-5 pr-5">
-              <vue-scroll :style="{ height: `${500}px` }" style="width: 100%">
-                <!-- Contacts -->
-                <v-list tile dense>
-                  <!-- Emails -->
-                  <v-list-item
-                    v-for="(email, emailIndex) in contact.emails"
-                    :key="`email-${emailIndex}`"
-                    ripple
-                    link
-                    selectable
-                  >
-                    <v-list-item-avatar size="30">
-                      <v-icon v-if="emailIndex === 0">mdi-email</v-icon>
-                    </v-list-item-avatar>
-                    <v-item-group>
-                      <v-list-item-title>{{ email.value }}</v-list-item-title>
-                      <v-list-item-subtitle>{{ email.type }}</v-list-item-subtitle>
-                    </v-item-group>
-                  </v-list-item>
-                  <!-- Phones -->
-                  <v-list-item
-                    v-for="(phone, phoneIndex) in contact.phone_numbers"
-                    :key="phoneIndex"
-                    ripple
-                    link
-                    selectable
-                  >
-                    <v-list-item-avatar size="30">
-                      <v-icon v-if="phoneIndex === 0">mdi-phone</v-icon>
-                    </v-list-item-avatar>
-                    <v-item-group>
-                      <v-list-item-title>{{ $libPhoneNumberJs.parsePhoneNumber(phone.value).formatNational() }}</v-list-item-title>
-                      <v-list-item-subtitle>{{ phone.type }}</v-list-item-subtitle>
-                    </v-item-group>
-                    <v-spacer />
-                    <v-item-group
-                    >
-                      <template v-if="['accepted'].includes($jsSIP.state) && $jsSIP.target === phone.value">
-                        {{ secondsToHms($jsSIP.seconds) }}
-                      </template>
-                    </v-item-group>
-                    <v-list-item-action>
-                      <v-btn
-                        v-if="['accepted', 'call', 'connecting', 'progress'].includes($jsSIP.state) && $jsSIP.target === phone.value"
-                        :key="phone.value"
-                        icon
-                        @click="$jsSIP.cancel()"
-                      >
-                        <v-icon color="red">mdi-phone-hangup</v-icon>
-                      </v-btn>
-                      <v-btn
-                        v-else
-                        icon
-                        :key="phone.value"
-                        :disabled="['accepted', 'call', 'connecting', 'progress'].includes($jsSIP.state) && $jsSIP.target !== phone.value"
-                        @click="onCall(phone.value, contact.id)"
-                      >
-                        <v-icon>mdi-phone</v-icon>
-                      </v-btn>
-                    </v-list-item-action>
-                  </v-list-item>
-                </v-list>
-                <v-divider />
-                <!-- History -->
-                <v-list
-                  tile
-                  dense
-                >
-                  <template v-if="contactHistory.length === 0">
-                    <v-list-item class="text-center">
-                      <v-spacer />
-                      <span class="grey--text">
-                        По этому контакту ещё нет не одного звонка
-                      </span>
-                      <v-spacer />
-                    </v-list-item>
-                  </template>
-                  <template v-else-if="contactDialog.history.loading">
-                    <v-list-item class="text-center">
-                      <v-spacer />
-                      <span class="grey--text">
-                        Загрузка содержимого...
-                      </span>
-                      <v-spacer />
-                    </v-list-item>
-                  </template>
-                  <template v-else>
-                    <v-list-item
-                      v-for="(item, index) in contactHistory"
-                      :key="index"
-                      ripple
-                      link
-                      selectable
-                      dense
-                    >
-                      <v-item-group>
-                        <v-list-item-avatar size="25">
-                          <v-icon
-                            v-if="item.direction === 0"
-                            color="red"
-                          >mdi-phone-missed
-                          </v-icon>
-                          <v-icon v-else-if="item.direction === 1">mdi-phone-incoming</v-icon>
-                          <v-icon v-else-if="item.direction === 2" color="red">mdi-phone-incoming</v-icon>
-                          <v-icon v-else-if="item.direction === 3">mdi-phone-outgoing</v-icon>
-                          <v-icon v-else-if="item.direction === 4" color="red">mdi-phone-outgoing</v-icon>
-                        </v-list-item-avatar>
-                      </v-item-group>
-                      <v-item-group class="mr-5">
-                        <v-list-item-title>{{ $libPhoneNumberJs.parsePhoneNumber(item.target).formatNational() }}</v-list-item-title>
-                      </v-item-group>
-                      <v-item-group>
-                        <v-list-item-title v-if="item.direction === 0">Пропущеный</v-list-item-title>
-                        <v-list-item-title v-else-if="item.direction === 1">Входящий {{ secondsToHms(item.duration) }}</v-list-item-title>
-                        <v-list-item-title v-else-if="item.direction === 2">Входящий отменён {{ secondsToHms(item.duration) }}</v-list-item-title>
-                        <v-list-item-title v-else-if="item.direction === 3">Исходящий {{ secondsToHms(item.duration) }}</v-list-item-title>
-                        <v-list-item-title v-else-if="item.direction === 4">Исходящий отменён {{ secondsToHms(item.duration) }}</v-list-item-title>
-                      </v-item-group>
-                      <v-spacer />
-                      <v-item-group>
-                        <v-list-item-title>{{ new Date(item.start_time * 1000).toLocaleString() }}</v-list-item-title>
-                      </v-item-group>
-                    </v-list-item>
-                  </template>
-                </v-list>
-              </vue-scroll>
-            </v-container>
-          </v-sheet>
-        </v-card>
-      </v-dialog>
-    </v-row>
   </div>
 </template>
 
