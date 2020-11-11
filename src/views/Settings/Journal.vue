7e797b7c6b8859f775c0000e94d3df2dd88a53a1
IGOR <igorpopryadukhin@gmail.com>
2020-11-11 10:13:56 +0300

    Journal example page

--- src/views/Settings/Journal.vue
+++ src/views/Settings/Journal.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>Journal</div>
+  <v-list>
+    <v-list-item link>
+      <v-list-item-content>
+        <v-list-item-title>Single-line item</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-list-item two-line link>
+      <v-list-item-content>
+        <v-list-item-title>Two-line item</v-list-item-title>
+        <v-list-item-subtitle>Secondary text</v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-list-item three-line link>
+      <v-list-item-content>
+        <v-list-item-title>Three-line item</v-list-item-title>
+        <v-list-item-subtitle>
+          Secondary line text Lorem ipsum dolor sit amet,
+        </v-list-item-subtitle>
+        <v-list-item-subtitle>
+          consectetur adipiscing elit.
+        </v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+  </v-list>
 </template>
 
 <script lang="ts">
