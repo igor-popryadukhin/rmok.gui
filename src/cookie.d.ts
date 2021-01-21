d97b10f3dd76bc8ac4f3c60f195a02f7f5f92ab8
IGOR <igorpopryadukhin@gmail.com>
2021-01-21 03:15:09 +0300

    Bugfix

--- src/cookie.d.ts
+++ src/cookie.d.ts
@@ -1,6 +1,5 @@
 // 1. Обязательно импортируйте Vue перед объявлением расширенных типов
-import Vue from 'vue'
-import {Cookie} from '@/plugins/cookie';
+import { Cookie } from '@/plugins/cookie'
 
 // 2. Укажите файл с типами, которые вы хотите расширить
 //    Vue имеет тип конструктора в types/vue.d.ts
