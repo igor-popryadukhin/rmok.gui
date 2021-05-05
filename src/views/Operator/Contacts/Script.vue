733928159465c2d254196e3c2441c664000af93d
Игорь Попрядухин <igorpopryadukhin@gmail.com>
2021-05-05 14:57:22 +0000

    Resolve "Сделать WYSIWYG редактор в поле скрипта, как в скорозвоне."

--- src/views/Operator/Contacts/Script.vue
+++ src/views/Operator/Contacts/Script.vue
@@ -1,16 +1,15 @@
 <template>
   <div class="overflow-auto">
-    <v-img
-      min-width="1500"
-      src="/images/7pwqamokup.png"
-    />
+    <v-editor />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import VEditor from '@/components/VEditor/VEditor.vue'
 
 export default Vue.extend({
+  components: { 'v-editor': VEditor },
   data () {
     return {
       history: [1, 2, 3, 4, 5]
