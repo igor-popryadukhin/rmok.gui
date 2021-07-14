dd20a7470c918a528e3a27ece2e8bf7f03786745
IGOR <igorpopryadukhin@gmail.com>
2021-07-14 10:35:59 +0300

    #337 Логирование ошибок SIP
    Если конечная точка нечего не возвращает, то void

--- src/api/SipErrors.ts
+++ src/api/SipErrors.ts
@@ -7,12 +7,12 @@ export default class SipErrors {
    * Отправка ошибок SIP (WEBRTC)
    * @param data
    */
-  public addLog (data: { message: string; context?: string }): Promise<number> {
-    return new Promise<number>((resolve, reject) => {
+  public addLog (data: { message: string; context?: string }): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
       $axios.post('/logs/sips', data)
         .then((response: AxiosResponse) => {
           if ([200, 204].includes(response.status)) {
-            return resolve(response.data)
+            return resolve()
           }
           throw new APIError(response.data)
         }).catch(reject)
