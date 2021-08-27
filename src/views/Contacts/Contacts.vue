2092c6be2c3eed13018e569d59206700aa5658ce
IGOR <igorpopryadukhin@gmail.com>
2021-08-27 13:35:36 +0300

    Исправить фильтр в контакт листе

--- src/views/Contacts/Contacts.vue
+++ src/views/Contacts/Contacts.vue
@@ -590,11 +590,19 @@ export default Vue.extend<Data, Methods, Computed, Props>({
 
       // Дата и время создания контакта (возможен диапазон разделённый запятой)
       if (Array.isArray(this.filterContactCreatedAt)) {
-        const contactCreatedAtStart = this.$dayjs(this.filterContactCreatedAt[0], 'YYYY-MM-DD')
-        const contactCreatedAtEnd = this.$dayjs(this.filterContactCreatedAt[1], 'YYYY-MM-DD')
+        let contactCreatedAtStart = this.$dayjs(this.filterContactCreatedAt[0], 'YYYY-MM-DD')
+        let contactCreatedAtEnd = this.$dayjs(this.filterContactCreatedAt[1], 'YYYY-MM-DD')
 
-        contactCreatedAtStart.set('hour', 0).set('minute', 0).set('second', 0)
-        contactCreatedAtEnd.set('hour', 23).set('minute', 59).set('second', 59)
+        contactCreatedAtStart = contactCreatedAtStart.set('hour', 0).set('minute', 0).set('second', 0)
+        contactCreatedAtEnd = contactCreatedAtEnd.set('hour', 23).set('minute', 59).set('second', 59)
+
+        params.contact_created_at = `${contactCreatedAtStart.unix()},${contactCreatedAtEnd.unix()}`
+      } else if (this.filterContactCreatedAt) {
+        let contactCreatedAtStart = this.$dayjs(this.filterContactCreatedAt, 'YYYY-MM-DD')
+        let contactCreatedAtEnd = this.$dayjs(this.filterContactCreatedAt, 'YYYY-MM-DD')
+
+        contactCreatedAtStart = contactCreatedAtStart.set('hour', 0).set('minute', 0).set('second', 0)
+        contactCreatedAtEnd = contactCreatedAtEnd.set('hour', 23).set('minute', 59).set('second', 59)
 
         params.contact_created_at = `${contactCreatedAtStart.unix()},${contactCreatedAtEnd.unix()}`
       }
