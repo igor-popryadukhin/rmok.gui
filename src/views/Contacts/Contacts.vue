00cf3386998d80c907b0baaab1fab4da29932531
IGOR <igorpopryadukhin@gmail.com>
2021-08-27 13:35:36 +0300

    Исправить фильтр в контакт листе

--- src/views/Contacts/Contacts.vue
+++ src/views/Contacts/Contacts.vue
@@ -597,6 +597,14 @@ export default Vue.extend<Data, Methods, Computed, Props>({
         contactCreatedAtEnd.set('hour', 23).set('minute', 59).set('second', 59)
 
         params.contact_created_at = `${contactCreatedAtStart.unix()},${contactCreatedAtEnd.unix()}`
+      } else if (this.filterContactCreatedAt) {
+        const contactCreatedAtStart = this.$dayjs(this.filterContactCreatedAt, 'YYYY-MM-DD')
+        const contactCreatedAtEnd = this.$dayjs(this.filterContactCreatedAt, 'YYYY-MM-DD')
+
+        contactCreatedAtStart.set('hour', 0).set('minute', 0).set('second', 0)
+        contactCreatedAtEnd.set('hour', 23).set('minute', 59).set('second', 59)
+
+        params.contact_created_at = `${contactCreatedAtStart.unix()},${contactCreatedAtEnd.unix()}`
       }
 
       // ----
